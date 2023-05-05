af501783762858c8ca5709058c831b667dfaff7b
Nusret Wazowski <maxiegothfemboi@gmail.com>
2023-05-05 15:58:12 +0200

    Removed error page padding

--- src/components/shared/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/shared/ErrorBoundary/ErrorBoundary.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 const ErrorBoundary: FC = () => {
   return (
-    <div style={{ width: '100%', textAlign: 'center', padding: '5rem' }}>
+    <div style={{ width: '100%', textAlign: 'center' }}>
       <h1>Not found</h1>
     </div>
   );
