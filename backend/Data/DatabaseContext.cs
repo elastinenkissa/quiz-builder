472bab7e03682cf2750352841b8b1c702fcfdedb
Nusret <nusretwazowski@gmail.com>
2023-12-17 21:09:07 +0100

    refactor(backend): change database to have many to many relationship with quizzes and questions

--- backend/Data/DatabaseContext.cs
+++ backend/Data/DatabaseContext.cs
@@ -16,5 +16,13 @@ namespace backend.Data
 
         public DbSet<Quiz> Quizzes { get; set; }
         public DbSet<Question> Questions { get; set; }
+        public DbSet<QuizQuestion> QuizQuestions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<QuizQuestion>().HasKey(qq => new { qq.QuizId, qq.QuestionId });
+            modelBuilder.Entity<QuizQuestion>().HasOne(q => q.Quiz).WithMany(qq => qq.QuizQuestions).HasForeignKey(q => q.QuizId);
+            modelBuilder.Entity<QuizQuestion>().HasOne(q => q.Question).WithMany(qq => qq.QuizQuestions).HasForeignKey(q => q.QuestionId);
+        }
     }
 }
